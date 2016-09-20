b3080d52f43050c6df60288e21e5934cc0c8e8a6
Chris Archibald <carchi8py@gmail.com>
2016-09-19 17:56:18 -0700

    Showing all Volcanos in the US

--- map.py
+++ map.py
@@ -1,8 +1,9 @@
 import folium
-map = folium.Map(location=[45.372,-121.697], zoom_start = 12, tiles = "Stamen Terrain")
+import pandas
 
-map.simple_marker(location = [45.3288, -121.6625], popup = "Mt. Hood", marker_color="red")
-map.simple_marker(location = [45.3311, -121.7311], popup = "Timberlake Lodge", marker_color="green")
+df = pandas.read_csv("Volcanoes-USA.txt")
+map = folium.Map(location=[45.372,-121.697], zoom_start = 6, tiles = "Stamen Terrain")
+for lat, lon, name in zip(df['LAT'], df['LON'], df['NAME']):
+    map.simple_marker(location = [lat,lon], popup = name, marker_color="red")
 
-#TODO: save is deprecated we need to fix this later
 map.save("test.html")
