899feb2132b6a062c34c9be45576018b92c7bc34
Chris Archibald <carchi8py@gmail.com>
2016-09-19 17:21:38 -0700

    Added a marker to the map

--- map.py
+++ map.py
@@ -1,3 +1,8 @@
 import folium
 map = folium.Map(location=[45.372,-121.697], zoom_start = 12, tiles = "Stamen Terrain")
+
+map.simple_marker(location = [45.3288, -121.6625], popup = "Mt. Hood", marker_color="red")
+map.simple_marker(location = [45.3311, -121.7311], popup = "Timberlake Lodge", marker_color="green")
+
+#TODO: save is deprecated we need to fix this later
 map.create_map(path="test.html")
