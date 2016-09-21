17ff18a7511fb58ecd8a3419421e057667f50704
Chris Archibald <carchi8py@gmail.com>
2016-09-21 16:57:55 -0700

    Change i forgot to check in

--- map.py
+++ map.py
@@ -3,7 +3,12 @@ import pandas
 
 df = pandas.read_csv("Volcanoes-USA.txt")
 map = folium.Map(location=[45.372,-121.697], zoom_start = 6, tiles = "Stamen Terrain")
-for lat, lon, name in zip(df['LAT'], df['LON'], df['NAME']):
-    map.simple_marker(location = [lat,lon], popup = name, marker_color="red")
+for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    color = 'red'
+    if elev in range(0,1000):
+        color = "green"
+    elif elev in range(1000,3000):
+        color = "orange"
+    map.simple_marker(location = [lat,lon], popup = name, marker_color=color)
 
 map.save("test.html")
