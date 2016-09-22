532f13281b50239ec04ea305ba4c3c6c79721665
Chris Archibald <carchi8py@gmail.com>
2016-09-21 17:18:16 -0700

    Made the icon inner color green

--- map.py
+++ map.py
@@ -12,6 +12,6 @@ def color(elev):
 df = pandas.read_csv("Volcanoes-USA.txt")
 map = folium.Map(location=[df['LAT'].mean(),df['LON'].mean()], zoom_start = 6, tiles = "Stamen Terrain")
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    map.add_child(folium.Marker(location = [lat,lon], popup = name, icon = folium.Icon(color=color(elev))))
+    map.add_child(folium.Marker(location = [lat,lon], popup = name, icon = folium.Icon(color=color(elev), icon_color="green")))
 
 map.save("test.html")
