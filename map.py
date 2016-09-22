220ea4cd0b7c030f751a7cf1f8e917cbbc8dab02
Chris Archibald <carchi8py@gmail.com>
2016-09-21 17:02:00 -0700

    Made a color function

--- map.py
+++ map.py
@@ -1,14 +1,17 @@
 import folium
 import pandas
 
+def color(elev):
+    color_name = 'red'
+    if elev in range(0,1000):
+        color_name = "green"
+    elif elev in range(1000,3000):
+        color_name = "orange"
+    return color_name
+
 df = pandas.read_csv("Volcanoes-USA.txt")
 map = folium.Map(location=[45.372,-121.697], zoom_start = 6, tiles = "Stamen Terrain")
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    color = 'red'
-    if elev in range(0,1000):
-        color = "green"
-    elif elev in range(1000,3000):
-        color = "orange"
-    map.simple_marker(location = [lat,lon], popup = name, marker_color=color)
+    map.simple_marker(location = [lat,lon], popup = name, marker_color=color(elev))
 
 map.save("test.html")
